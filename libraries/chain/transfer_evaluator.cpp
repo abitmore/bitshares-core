4bd46e07f23172eddb2a663e44d0c986e42a9d57
abitmore <abitmore@users.noreply.github.com>
2016-02-15 06:59:26 +0800

    Expand insufficient fee error message.

--- libraries/chain/transfer_evaluator.cpp
+++ libraries/chain/transfer_evaluator.cpp
@@ -40,6 +40,7 @@ void_result transfer_evaluator::do_evaluate( const transfer_operation& op )
       GRAPHENE_ASSERT( core_fee_paid + max_fees_payable_with_coin_seconds >= required_core_fee,
                     insufficient_fee,
                     "Insufficient Fee Paid",
+                    ("payable_from_coin_seconds", max_fees_payable_with_coin_seconds)
                     ("core_fee_paid",core_fee_paid)("required",required_core_fee) );
       // if some fees are paid with coin seconds
       if( core_fee_paid < required_core_fee )
@@ -117,6 +118,7 @@ void_result transfer_v2_evaluator::do_evaluate( const transfer_v2_operation& op 
       GRAPHENE_ASSERT( core_fee_paid + max_fees_payable_with_coin_seconds >= required_core_fee,
                     insufficient_fee,
                     "Insufficient Fee Paid",
+                    ("payable_from_coin_seconds", max_fees_payable_with_coin_seconds)
                     ("core_fee_paid",core_fee_paid)("required",required_core_fee) );
       // if some fees are paid with coin seconds
       if( core_fee_paid < required_core_fee )
